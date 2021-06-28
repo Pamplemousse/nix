3659172537d158071d9a9d23d823edad3f63e6b9
Pamplemousse <xav.maso@gmail.com>
2021-06-28 15:10:31 -0700

    Fuzz: Update run command
      * limit memory as in "production" (OSS-fuzz later)
      * put results in dedicated folder
    
    Signed-off-by: Pamplemousse <xav.maso@gmail.com>

--- scripts/run-fuzzer.sh
+++ scripts/run-fuzzer.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-ASAN_OPTIONS="detect_leaks=false" ./fuzzer fuzz/corpus/ fuzz/seeds/
+# OSS-fuzz has a limit of 2.5GB before treating an input as an Out-Of-Memory bug:
+# https://google.github.io/oss-fuzz/faq/#how-do-you-handle-timeouts-and-ooms
+MAX_MEMORY=2500
+
+ASAN_OPTIONS="detect_leaks=false" ./fuzzer \
+  -artifact_prefix="fuzz/results/" \
+  -rss_limit_mb="$MAX_MEMORY" \
+  -fork=4 \
+  fuzz/corpus/ fuzz/seeds/
